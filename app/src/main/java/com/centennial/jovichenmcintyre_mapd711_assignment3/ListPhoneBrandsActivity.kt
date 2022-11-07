2704aec09483cebca53ef86c3258d5aead53e807
jovi848 <jovi848.mcintyre@gmail.com>
2022-11-06 18:57:45 -0600

    add new activity

---
+++ app/src/main/java/com/centennial/jovichenmcintyre_mapd711_assignment3/ListPhoneBrandsActivity.kt
@@ -0,0 +1,11 @@
+package com.centennial.jovichenmcintyre_mapd711_assignment3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPhoneBrandsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_phone_brands)
+    }
+}
